5d65ad0d87348ec2cbcdc02450798e1c72cc5782
NakataNikolov <atanasvolokin@gmail.com>
2020-03-16 12:09:23 +0200

    Delasport Task

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -13,42 +13,14 @@ export class ApiService {
       'Authorization': 'Basic' + btoa('hard:hard')
     })
   }
-  //returns all categories
-  public getCategories(){
+  //returns movies
+  public getMovies(){
     var username = 'hard';
     var password = 'hard';
    // const credentials = username+':'+password;
     let headers = new HttpHeaders();
    // headers = headers.append("Authorization", "Basic " + btoa(credentials));
     //headers = headers.append("Content-Type", "application/x-www-form-urlencoded");
-    return this.httpClient.get('https://localhost:44397/api/Categories',{headers: headers});
-  }
-  //makes new category
-  public createNewCategory(categoryObj){
-    return this.httpClient.post('https://localhost:44397/api/Categories',categoryObj);
-  }
-  //delete category 
-  public deleteCategory(categoryId){
-    return this.httpClient.delete('https://localhost:44397/api/Categories/'+categoryId, {responseType: 'text'});
-  }
-  //update exsiting category
-  public updateCategory(categoryObj,categoryId){
-    return this.httpClient.put('https://localhost:44397/api/Categories/'+categoryId, categoryObj);
-  }
-  //retrive products from db
-  getProducts(){
-    var username = 'hard';
-    var password = 'hard';
-   // const credentials = username+':'+password;
-    let headers = new HttpHeaders();
-   // headers = headers.append("Authorization", "Basic " + btoa(credentials));
-    //headers = headers.append("Content-Type", "application/x-www-form-urlencoded");
-    return this.httpClient.get('https://localhost:44397/api/Stocks',{headers: headers});
-  }
-  createNewProduct(productObj){
-    return this.httpClient.post('https://localhost:44397/api/Stocks',productObj);
-  }
-  deleteProduct(productId){
-    return this.httpClient.delete('https://localhost:44397/api/Stocks/'+productId,{responseType: 'text'})
+    return this.httpClient.get('https://reactnative.dev/movies.json',{headers: headers});
   }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,7 @@
 import { Component } from '@angular/core';
 import { ApiService } from './api.service';
-import {NgxPaginationModule} from 'ngx-pagination';
-import { AngularWaitBarrier } from 'blocking-proxy/built/lib/angular_wait_barrier';
 import $ from 'jquery';
 import {PersistdataService} from './persistdata.service'
-import { NgForm } from '@angular/forms';
-import { privateEncrypt } from 'crypto';
-
 
 @Component({
   selector: 'app-root',
@@ -15,185 +10,54 @@ import { privateEncrypt } from 'crypto';
 })
 export class AppComponent {
   title = 'job-aplication-angular';
-  products;
-  totalRec : number;
-  page: number = 1;
-  categories;
-  cartArr = [];
-  cartAmount;
-  totalPrice = 0;
-  discount;
+  moviesArray;
+  cResult : number;
+  fResult : number;
+  aCellValue : number;
+  bCellValue : number;
+  dCellValue : number;
+  eCellValue : number;
+  adbeMax : number;
+  adbeMin : number;
+  cfghAvg : number; 
   constructor(
     private apiService: ApiService,
-    private dataColection: PersistdataService
+    private dataColection: PersistdataService,
   ) { 
   }
   ngOnInit() {
- 
-    this.getCategories();
-    this.getProducts();
-    this.cartAmountfunc();
+    this.getMovies();
   }
   //retrive all products from db 
-  getProducts(){
-    var categoryName ;
-    this.apiService.getProducts().subscribe((data)=>{
-      this.products = data;
-      this.totalRec = this.products.length;
-      for(var i in this.products){
-        categoryName = this.categories.filter(x => x.id ===  this.products[i].category);
-        this.products[i].categoryName = categoryName[0].name;
-      }
-    })
-
-  }
-  //create new product 
-  createProduct(form : NgForm){
-      var newProductObj = {
-        name: form.value.product_name,
-        description: form.value.product_description,
-        price: parseFloat(form.value.product_price),
-        quantity: parseInt(form.value.available_quantity),
-        category: parseInt(form.value.product_category)
-      };
-      this.apiService.createNewProduct(newProductObj).subscribe((data)=>{
-        this.closeModal();
-        this.getProducts();
-      })
-    }
-  //remove product from db
-  removeProduct(productId){
-    this.apiService.deleteProduct(productId).subscribe((data) =>{
-      this.getProducts();
+  getMovies(){
+    var rawData;
+    this.apiService.getMovies().subscribe((data)=>{
+      rawData = data;
+      this.moviesArray = rawData.movies;
+      console.log(this.moviesArray);
     })
   }
-  //retrive all categories from db
-  getCategories(){
-      this.apiService.getCategories().subscribe((data)=>{
-        this.categories= data;
-        this.totalRec = this.categories.length;
-      })
-  }
-  //makes new category 
-  createCategory(form : NgForm){
-    var newCategoryObj = {
-      name: form.value.category_name,
-      description: form.value.categorie_description,
-      available: form.value.category_available,
-    };
-    this.apiService.createNewCategory(newCategoryObj).subscribe((data)=>{
-      this.closeCategoryModal();
-      this.getCategories();
-    })
-  }
-  // delete category from db 
-  removeCategory(categoryId){
-    this.apiService.deleteCategory(categoryId).subscribe((data)=>{
-      this.getCategories();
-    })
-  }
-  //opens model that helps to add new category 
-  showCategoryModal(){
-    var modal = $('#categoriesModal')[0];
-    modal.style.display = "block";
-  }
-  //shows modal that helps removing categories
-  showDeleteCategory(){
-    var modal = $('#removeCategoriesModal')[0];
-    modal.style.display = 'block';
-  }
-  //edit category 
-  editCategory(categoryID){
-    var categoryForm = $('#editCategoriesForm'+ categoryID)[0];
-    categoryForm.style.display = 'block';
-  }
-  updateCategory(form : NgForm, categoryId){
-
-    var specificCategory = this.categories.filter(x => x.id === categoryId);
-    specificCategory[0].name = form.value.update_category_name;
-    specificCategory[0].description = form.value.update_categorie_description;
-    specificCategory[0].available = form.value.update_category_available;
-    this.apiService.updateCategory(specificCategory[0],categoryId).subscribe((data)=>{
-      this.getCategories();
-    })
-  }
-  expandImage() {
-    var modal = $('#myModal')[0];
-    modal.style.display = "block";
-  }
-  closeCategoryModal(){
-    var modal = $('#categoriesModal')[0];
-    modal.style.display = "none";
-  }
-  closeCategoryForm(categoryID){
-    var categoryForm = $('#editCategoriesForm'+ categoryID)[0];
-    categoryForm.style.display = 'none';
-  }
-  closeModal(){
-    var modal = $('#myModal')[0];
-    modal.style.display = "none";
-  }
-  closeRemoveCategoryModal(){
-    var modal = $('#removeCategoriesModal')[0];
-    modal.style.display = 'none';
-  }
-  //used for activating remove product functionality
-  removeProductFunc(){
-    var productOptions = $('.container').find('.edit-items');
-    if(productOptions.hasClass('hide-edit-products')){
-      productOptions.removeClass('hide-edit-products');
-      productOptions.addClass('show-edit-products');
-    }else{
-      productOptions.removeClass('show-edit-products');
-      productOptions.addClass('hide-edit-products');
+  calcCbox(){
+    if(this.aCellValue && this.bCellValue){
+      this.cResult = this.aCellValue + this.bCellValue; 
+      this.grandCalc();
     }
   }
-  //changing qty of the selected product
-  changeQty(productId, step, maxQty){
-    var quantity = parseInt($('#cart-quantity'+ productId).val());
-    if(step){
-      if(maxQty > quantity)
-      $('#cart-quantity' + productId).val(quantity + 1);
-    }else{
-      if(quantity>0 ){
-        $('#cart-quantity' + productId).val(quantity - 1);
-      }
-    }
-
-  }
-  //adding to cart array 
-  addToCart(productId){
-      var quantity = parseInt($('#cart-quantity'+ productId).val());
-      var specificProduct = this.products.filter(x => x.id === productId);
-     if(quantity > 0){
-      this.cartArr.push({
-        prodId: productId,
-        prodName: specificProduct[0].name +" "+ specificProduct[0].description,
-        prodPrice: specificProduct[0].price * quantity,
-        qty:quantity
-      });
-      this.cartAmountfunc();
-     }
-  }
-  //adding products in cart
-  cart(){
-    this.totalPrice = 0;
-    this.discount = 5;
-    var finalPrice = 0;
-    if(this.cartArr.length > 0){
-      for(var i in this.cartArr){
-        finalPrice += this.cartArr[i].prodPrice;
-      }
-      if(this.cartArr.length > 1){
-        this.totalPrice = finalPrice - ((finalPrice * this.discount)/100);
-      }else{
-        this.totalPrice = finalPrice;
-      }
+  calcFbox(){
+    if(this.dCellValue && this.eCellValue){
+      this.fResult = this.dCellValue * this.eCellValue;
+      this.grandCalc();
     }
   }
-  //showing the current amount of products in cart
-  cartAmountfunc(){
-    this.cartAmount = 0;
-    this.cartAmount = this.cartArr.length;
+  grandCalc(){
+    var cellValuesArray = new Array();
+    var sumCells = 0;
+    if(this.aCellValue && this.bCellValue && this.dCellValue && this.eCellValue){
+      cellValuesArray.push(this.aCellValue, this.bCellValue, this.dCellValue, this.eCellValue);
+      this.adbeMax = Math.max(... cellValuesArray);
+      this.adbeMin = Math.min(... cellValuesArray);
+      sumCells += cellValuesArray.reduce(function(a, b) { return a + b; }, 0);;
+      this.cfghAvg = (sumCells) / cellValuesArray.length;
+    }
   }
 }
